cb63f320b1835cabbfa0e0de636fa61ea233b3fb
thepi <thepigs@gmail.com>
2020-09-02 22:44:53 +1000

    xx

---
+++ main.js
@@ -0,0 +1,41 @@
+import 'ol/ol.css';
+import MVT from 'ol/format/MVT';
+import Map from 'ol/Map';
+import VectorTileLayer from 'ol/layer/VectorTile';
+import VectorTileSource from 'ol/source/VectorTile';
+import View from 'ol/View';
+import {Fill, Stroke, Style} from 'ol/style';
+
+const zoom = 14.493205628579398
+const center = [-225002.55911738425, 1433313.650332927]
+
+var country = new Style({
+    stroke: new Stroke({
+        color: 'gray',
+        width: 1,
+    }),
+    fill: new Fill({
+        color: 'rgba(255,255,255,0.5)',
+    }),
+});
+var vtLayer = new VectorTileLayer({
+
+    source: new VectorTileSource({
+        maxZoom: 15,
+        format: new MVT({
+            idProperty: 'iso_a3',
+        }),
+        url:
+            'https://ahocevar.com/geoserver/gwc/service/tms/1.0.0/' +
+            'ne:ne_10m_admin_0_countries@EPSG%3A900913@pbf/{z}/{x}/{-y}.pbf',
+    }),
+    style: country,
+});
+
+var map = new Map({
+    layers: [vtLayer],
+    target: 'map',
+    view: new View({
+        center, zoom
+    }),
+});
